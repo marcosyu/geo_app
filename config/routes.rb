415028fdfe4e7c6931994910b1839a04f1f4af90
marcosyu <marcosyu26@gmail.com>
2019-07-10 20:09:42 -0700

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # constraints subdomain: "api" do
-  #   scope module: "api" do
-  #     namespace :v1, defaults: { format: :json } do
-  #       resources :location, only: [:index, :create, :show]
-  #     end
-  #   end
-  # end
+
   constraints subdomain: "api" do
     scope module: "api" do
       namespace :v1, defaults: { format: 'json' } do
