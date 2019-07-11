a7166a9ed382aacdd0a8798f33c39f15a5682c1b
marcosyu <marcosyu26@gmail.com>
2019-07-10 20:43:45 -0700

    added config for redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  constraints subdomain: "api" do
-    scope module: "api" do
-      namespace :v1, defaults: { format: 'json' } do
-        resources :locations, only: [:index, :create, :show]
-      end
+
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :locations, only: [:index, :create, :show]
     end
   end
   mount Resque::Server.new, :at => "/resque"
